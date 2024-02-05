d77f96c8d2701bdaf5530047d35b15dad2bef782
CORREIA Lucie <lucie.correia@telecomnancy.eu>
2024-02-05 23:19:52 +0100

    ex1

---
+++ ex1.js
@@ -0,0 +1,37 @@
+function f(a, b) {
+    const sum = a + b;
+    return sum;
+}
+console.log(f(3, 4)); // 7
+//  OU
+var g = function(a, b) {
+    const sum = a + b;
+    return sum;
+}
+console.log(g(3, 8)); // 11
+//  OU
+const h = (a, b) => {
+    const sum = a + b;
+    return sum;
+};
+console.log(h(3, 4)); // 7
+//  OU One-liner
+const j = (a, b) => a + b;
+console.log(j(3, 4)); // 7
+
+// Écrire une fonction qui renvoie hello world
+var createHelloWorld = function() {
+    return function() {
+        return "Hello World";
+    }
+};
+var helloWorldFunction = createHelloWorld();
+var result = helloWorldFunction();
+console.log(result);  // Cela affichera "Hello World" dans la console
+
+var helloWorld = function() {
+    console.log("Hello World");
+};
+
+// Appel de la fonction pour afficher "Hello World"
+helloWorld();
