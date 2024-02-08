c606cd7e66668eb5feb28b78634d75e19da20299
CORREIA Lucie <lucie.correia@telecomnancy.eu>
2024-02-08 18:45:21 +0100

    js

--- ex2.js
+++ ex2.js
@@ -3,9 +3,16 @@ n = 10
 ["call","call","call"]
 Output: [10,11,12] */
 
+var createCounter = function(n) {
+    let currentCount = n - 1;
+    return function() {
+      currentCount += 1;
+      return currentCount;      
+    };
+  };
+
+
 // CLOSURE
-
-
 function createAdder(a) {
     return function add(b) {
       const sum = a + b;
@@ -15,5 +22,4 @@ function createAdder(a) {
   
   const addTo2 = createAdder(2); // addTo2 est donc une fonction qui ajoute 2 à un nombre
   addTo2(5); // 7
-
 // ici on crée une fonctionqui prend une fonction en argument.
